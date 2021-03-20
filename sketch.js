d7c112fbc70b9f246eb3443eeb3e0a706a6187ec
neelgupta123 <75940404+neelgupta123@users.noreply.github.com>
2021-03-20 11:33:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -35,7 +35,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.7, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.6, isStatic:true});
 	World.add(world, packageBody);
 	
 
